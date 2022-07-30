62d725bb20b8cf0e688a086a209b4d3f2df6fae1
nadtosonna <prasolova.kateryna@gmail.com>
2022-07-30 14:53:40 +0300

    all logic added, styles in process

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,6 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`)
-        .then(response => {
-            if (response.satus === 404) {
-                throw new Error(response.status);
-            } else return response.json();
-    })
+    return fetch(`${BASE_URL}${name}?fields=name,capital,population,languages,flags`)
+        .then(response => response.json());
 } 
--- src/index.js
+++ src/index.js
@@ -3,40 +3,67 @@ import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
 const refs = {
     input: document.querySelector('#search-box'),
     countryList: document.querySelector('.country-list'),
     countryCard: document.querySelector('.country-info')
 }
+let countryPull = '';
 
 refs.input.addEventListener('input', debounce(onInputCountrySearch, DEBOUNCE_DELAY));
 
 function onInputCountrySearch(event) {
+    clearUI();
     const name = refs.input.value.trim();
-    
+
     if (name === '') return;
 
     fetchCountries(name)
-        .then(response => {
-            console.log(response);
-            if (response.length > 10) Notify.info('Too many matches found. Please enter a more specific name.');
-            if (response.length > 1 && response.length < 10) {
-                renderCountriesList();
+        .then(countries => {
+            if (countries.length >= 10) Notify.info('Too many matches found. Please enter a more specific name.');
+            else if (countries.length === 1) {
+                refs.countryCard.innerHTML = renderCountryCard(countries[0]);
             }
-            if (response.length === 1) {
-                renderCountryCard();
-            }
+            else renderCountriesList(countries);
         })
-        .catch((error) => Notify.failure('Oops, there is no country with that name'))
-}
-    
-function renderCountriesList(response) {
-    return `<li>
-    <p>${response.name.common}</p>
-    </li>`;
+        .catch(error => Notify.failure('Oops, there is no country with that name'))
 }
 
-function renderCountryCard(response) {
+function clearUI() {
+    refs.countryList.innerHTML = '';
+    refs.countryCard.innerHTML = '';
+}
 
+function renderCountriesList(countries) {
+    countries.forEach(country => {
+    countryPull += `<li class="country-list__item">
+                        <img src="${country.flags.svg}" class="country-list__flag" width="70px" height="40px">
+                        <p class="country-list__name">${country.name.common}</p>
+                    </li>`;  
+    });
+    refs.countryList.innerHTML = countryPull;
+}
+
+function renderCountryCard( {name, capital, population, languages, flags} ) {
+    return `
+    <div class="country-info__card">
+    <h2 class="country-info__name">${name.common}</h2>
+    <img src="${flags.svg}" class="country-info__flag" width="240px" height="160px">
+    <ul class="country-info__features">
+        <li>
+            <h3>Capital:&nbsp;</h3>
+            <p>${capital}</p>
+        </li>
+        <li>
+            <h3>Population:&nbsp;</h3>
+            <p>${population}</p>
+        </li>
+        <li>
+            <h3>Languages:&nbsp;</h3>
+            <p>${languages}</p>
+        </li>
+    </ul>
+    </div>
+    `
 }
